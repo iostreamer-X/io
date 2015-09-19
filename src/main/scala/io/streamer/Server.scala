ed2b5f03c5879b82a60e3cbe22d323713934e584
iostreamer <iostreamer007@gmail.com>
2015-09-19 18:59:15 +0530

    Shifted from actor approach to Rx

--- src/main/scala/io/streamer/Server.scala
+++ src/main/scala/io/streamer/Server.scala
@@ -10,27 +10,27 @@ class Server[T](port:Int, execute:Socket => T) {
 	def start()={
 		Observable[T](
 			observer => {
-					new Thread {
-						override def run() = {
-							val serverSocket = new ServerSocket(port)
-							while(true){
-								val socket = serverSocket.accept()
-								new Thread{
-									override def run() = {
-										try{
-											observer.onNext(execute(socket))
-										}
-										catch{
-											case t:Throwable =>
-												socket.close
-										}
+				new Thread {
+					override def run() = {
+						val serverSocket = new ServerSocket(port)
+						while(true){
+							val socket = serverSocket.accept()
+							new Thread{
+								override def run() = {
+									try{
+										observer.onNext(execute(socket))
 									}
-								}.start
-							}
+									catch{
+										case t:Throwable =>
+											socket.close
+									}
+								}
+							}.start
 						}
-					}.start()
-				}
-			)
+					}
+				}.start()
+			}
+		)
 		
 	}
 }
