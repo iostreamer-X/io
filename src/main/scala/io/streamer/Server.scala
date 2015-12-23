1a319a6e05386860cbf6a8e5c8862ee3ff38e7a1
iostreamer <iostreamer007@gmail.com>
2015-12-23 19:55:25 +0530

    Bug fix

--- src/main/scala/io/streamer/Server.scala
+++ src/main/scala/io/streamer/Server.scala
@@ -1,36 +1,34 @@
-package io.streamer
+package labs.amethyst.TheAmethyst
 
-import akka.actor._
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 import java.net.{ServerSocket, Socket}
-import rx.lang.scala.{JavaConversions, Observable}
 
-class Server[T](port:Int, execute:Socket => T) {
-	def start()={
-		val socket = serverSocket.accept()
+import rx.lang.scala.Observable
+
+class Server[T](port: Int, execute: Socket => T) {
+	def start() = {
+		val serverSocket = new ServerSocket(port)
 		Observable[T](
 			observer => {
 				new Thread {
 					override def run() = {
-						val serverSocket = new ServerSocket(port)
-						while(true){
-							new Thread{
+						while (true) {
+							val socket = serverSocket.accept()
+							new Thread {
 								override def run() = {
-									try{
+									try {
 										observer.onNext(execute(socket))
 									}
-									catch{
-										case t:Throwable =>
-											socket.close
+									catch {
+										case t: Throwable =>
+											socket.close()
 									}
 								}
-							}.start
+							}.start()
 						}
 					}
 				}.start()
 			}
 		)
-		
+
 	}
 }
